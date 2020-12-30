21512f906b98c662fd00ff0ccdae23e375b580b0
Dragon <ruuddyd@gmail.com>
2020-12-30 15:33:27 +0900

    User agent setting

--- Usragent.py
+++ Usragent.py
@@ -1,5 +1,5 @@
 import requests
-url = "http://nadocoding.tistory.com" # for certification
+url = "http://nadocoding.tistory.com" # for certification user
 headers = {"User-Agent" : "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_1_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36"}
 
 res = requests.get(url, headers = headers)
