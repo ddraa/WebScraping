b13d0425d6188deaffed522685b665d504795894
Dragon <ruuddyd@gmail.com>
2021-01-01 21:32:26 +0900

    selenium avoid captcha

--- seleniums.py
+++ seleniums.py
@@ -8,16 +8,29 @@ elem = browser.find_element_by_class_name("link_login")
 elem.click()
 
 # input id, pw
-browser.find_element_by_id("id").send_keys("id")
-browser.find_element_by_id("pw").send_keys("pw")
+#browser.find_element_by_id("id").send_keys("id")
+#browser.find_element_by_id("pw").send_keys("pw")
 
-# login button click
+
+# avoid naver captcha , login
+input_js = ' \
+        document.getElementById("id").value = "{id}"; \
+        document.getElementById("pw").value = "{pw}"; \
+    '.format(id = "this.id", pw = "this.pw")
+
+browser.execute_script(input_js)
 browser.find_element_by_id("log.login").click()
 
-browser.find_element_by_id("id").clear()
-browser.find_element_by_id("id").send_keys("my_new_id")
 
-print(browser.page_source)
 
-browser.quit()
+# login button click
+#browser.find_element_by_id("log.login").click()
+
+# erase and input
+#browser.find_element_by_id("id").clear()
+#browser.find_element_by_id("id").send_keys("my_new_id")
+
+#print(browser.page_source)
+
+#browser.quit()
 
